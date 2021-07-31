095b95e8b68e35eeb322c9916542f2c215b7c157
iodoyo16 <durfbgksmsskfro@gmail.com>
2021-07-31 16:32:59 +0900

    nodemon, dom 구조체와 fetch

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,20 @@
 "use strict";
 
-console.log('hello');
+//DOM -> Document Object Model 일종의 인터페이스.
+
+const id = document.querySelector("#id");
+const password=document.querySelector("#password");
+const loginBtn=document.querySelector("button");
+
+loginBtn.addEventListener("click",handleLoginBtnClick);
+
+function handleLoginBtnClick(){
+    const req={
+        id: id.value,
+        password:password.value,
+    };
+    fetch("/login",{
+        method: "POST"
+        body: JSON.stringify(req)
+    })
+}
