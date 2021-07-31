83fba7cf2458d40ec2204fc01a786d5b0a198ddc
iodoyo16 <durfbgksmsskfro@gmail.com>
2021-07-31 15:29:12 +0900

    폴더 구조 최적화

---
+++ app/app.js
@@ -0,0 +1,31 @@
+
+////////////////////////
+// setting
+///////////////////////
+
+//module
+const express=require('express');
+const app=express();
+
+
+//routing
+const home=require("./src/routes/home");
+
+
+// view setting
+app.set('views',__dirname+'/views');
+app.set('view engine','ejs');
+app.engine('html',require('ejs').renderFile); // .html 파일을 ejs 로 해석
+
+app.use("/",home);
+//app.use(express.static(__dirname+'/public'));
+
+
+
+
+const { PRIORITY_LOW } = require('constants');
+const path=require('path');
+
+
+module.exports=app;
+
