4dbe59f03c800d6c76e86f1819adec2d95d2e2b1
iodoyo16 <durfbgksmsskfro@gmail.com>
2021-08-01 23:50:20 +0900

    register fileDB, async await적용

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -2,16 +2,18 @@
 
 const fs=require("fs").promises;
 class UserStorage{
-    static getUsers(...fields){// field 명으로 전체 유저의 field 배열을 가져옴
+    static getUsers(isAll, ...fields){// field 명으로 전체 유저의 field 배열을 가져옴
         return fs
         .readFile("./src/databases/users.json")
             .then((data)=>{
-                return this.#getaskedField(data, fields);
+                return this.#getaskedField(data,isAll, fields);
             })
             .catch(console.error);
     };
-    static #getaskedField(data, fields){
+    static #getaskedField(data,isAll, fields){
         const users=JSON.parse(data);
+        if(isAll)return users;
+
         const askedFileds=fields.reduce((askedFileds,field)=>{
             if(users.hasOwnProperty(field)){// 해당 키값이 있는지
                 askedFileds[field]=users[field];
@@ -38,11 +40,20 @@ class UserStorage{
         },{});
         return askedUserInfo;
     }
-    static save(userinfo){
-        //const users=this.#users;
+    static async save(userInfo){
+        // 데이터 가져오기
+        const users=await this.getUsers(true);
+        if(users.id.includes(userInfo.id)){
+            console.log("1111");
+            return {success:false, msg:"이미 존재하는 아이디 입니다."};
+        }
+        // 데이터 추가
         users.id.push(userInfo.id);
-        users.id.push(userInfo.name);
-        users.id.push(userInfo.password);
+        users.password.push(userInfo.password);
+        users.name.push(userInfo.name);
+        fs.writeFile("./src/databases/users.json",JSON.stringify(users));
+        console.log("2222");
+        return {success:true};
     };
 
 }
